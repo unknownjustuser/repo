fc8131e6cb83b9bed7e9350f868e31a43c18938e
unknownjustuser <unknown.just.user@proton.me>
2024-03-18 08:15:45 +0530

    update

--- x86_64/update-db.sh
+++ x86_64/update-db.sh
@@ -10,6 +10,9 @@
 # get hidden bugs that are hard to discover.
 set -euo pipefail
 
+# echo "#################"
+# echo "Changing the dir."
+# echo "#################"
 # cd x86_64/
 
 echo "###########################"
@@ -17,8 +20,40 @@ echo "Building the repo database."
 echo "###########################"
 
 ## Arch: x86_64
-if [ -f "archfiery_repo"* ]; then
-  rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig
+if [[ ! -f "archfiery_repo.db" ]]; then
+  rm -f *.db
+fi
+
+if [[ ! -f "archfiery_repo.db.sig" ]]; then
+  rm -f *.db.sig
+fi
+
+if [[ ! -f "archfiery_repo.db.tar.gz" ]]; then
+  rm -f *.db.tar.gz
+fi
+
+if [[ ! -f "archfiery_repo.db.tar.gz.sig" ]]; then
+  rm -f *.db.tar.gz.sig
+fi
+
+if [[ ! -f "archfiery_repo.files" ]]; then
+  rm -f *.files
+fi
+
+if [[ ! -f "archfiery_repo.files.sig" ]]; then
+  rm -f *.files.sig
+fi
+
+if [[ ! -f "archfiery_repo.files.tar.gz" ]]; then
+  rm -f *.files.tar.gz
+fi
+
+if [[ ! -f "archfiery_repo.files.tar.gz.sig" ]]; then
+  rm -f *.files.tar.gz.sig
+fi
+
+if [[ ! -f "*.old" ]]; then
+  rm -f *.old
 fi
 
 echo "###################################"
@@ -32,45 +67,46 @@ echo "###################################"
 repo-add --verify --sign -R archfiery_repo.db.tar.gz *.pkg.tar.zst
 
 # Removing the symlinks.
-#echo "###################################"
-#echo "       Removing the symlinks.      "
-#echo "###################################"
-#
-#files=(
-# "archfiery_repo.db*"
-# "archfiery_repo.files*"
-#)
-#
-#for file in "${files[@]}"; do
-#  if [ -f "$file" ]; then
-#    echo "Removing $file"
-#    rm "$file"
-#  fi
-#done
+echo "######################"
+echo "Removing the symlinks."
+echo "######################"
+
+files=(
+  "archfiery_repo.db"
+  "archfiery_repo.db.sig"
+  "archfiery_repo.files"
+  "archfiery_repo.files.sig"
+)
+
+for file in "${files[@]}"; do
+  if [ -f "$file" ]; then
+    echo "Removing $file"
+    rm "$file"
+  fi
+done
 
 # Renaming the tar.gz files without the extension.
-#echo "################################################"
-#echo "Renaming the tar.gz files without the extension."
-#echo "################################################"
-#
-#files=(
-#  "archfiery_repo.db.tar.gz:archfiery_repo.db"
-#  "archfiery_repo.db.tar.gz.sig:archfiery_repo.db.sig"
-#  "archfiery_repo.files.tar.gz:archfiery_repo.files"
-#  "archfiery_repo.files.tar.gz.sig:archfiery_repo.files.sig"
-#)
-#
-#for file in "${files[@]}"; do
-#  old_name=$(echo "$file" | cut -d: -f1)
-#  new_name=$(echo "$file" | cut -d: -f2)
-#
-#  if [ -f "$old_name" ]; then
-#    echo "Renaming $old_name to $new_name"
-#    mv "$old_name" "$new_name"
-#  fi
-#done
+echo "################################################"
+echo "Renaming the tar.gz files without the extension."
+echo "################################################"
+
+files=(
+  "archfiery_repo.db.tar.gz:archfiery_repo.db"
+  "archfiery_repo.db.tar.gz.sig:archfiery_repo.db.sig"
+  "archfiery_repo.files.tar.gz:archfiery_repo.files"
+  "archfiery_repo.files.tar.gz.sig:archfiery_repo.files.sig"
+)
+
+for file in "${files[@]}"; do
+  old_name=$(echo "$file" | cut -d: -f1)
+  new_name=$(echo "$file" | cut -d: -f2)
+
+  if [ -f "$old_name" ]; then
+    echo "Renaming $old_name to $new_name"
+    mv "$old_name" "$new_name"
+  fi
+done
 
 echo "#######################################"
 echo "Packages in the repo have been updated!"
 echo "#######################################"
-
